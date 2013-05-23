1b376e0ae7156ea2a48fb559d9b4ed9c41278a6f
Kirk Stork <kastork@nps.edu>
2013-05-23 08:33:31 -0700

    Removed undesired interface for login/out

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
+import edu.nps.fauxgame.Role
+import edu.nps.fauxgame.User
+import edu.nps.fauxgame.UserRole
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+
+    def u = new User(usernname: 'kastork@nps.edu',
+                     password: 'foo',
+                     enabled: true,
+                     accountExpired: false,
+                     accountLockec: false,
+                     passwordExpired: false).save(flush: true)
+
+    def r = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+
+    def ur = new UserRole(user: u, role: r).save(flush: true)
+  }
+
+  def destroy = {
+  }
 }
